1146f1e2cff53663749548e8ce76654995161fa6
Michael Schwarze <michael@schwarze-web.de>
2015-11-02 22:16:28 +0100

    Bugfixing: Datum Kontoauszug; Refactoring.
    Das Kontoauszugsdatum wurde bislang aus dem ersten Datensatz genommen, was insb. nach der Umstellung der Sortierung dazu geführt hat, dass dieses Datum i.d.R. vom Anfang des Monats resultierte. Hier wird nun entsprechend das Datum des letzten Datensatzes der .CSV-Input-Datei genommen, was i.d.R. auch auf dem letzten Buchungstag liegt, was dem Kontoauszugsdatum in den meisten Fällen entspricht.
    
    Weiteres Refactoring gemäß Ruby Style Guide, insb. kleinere Methode / Separation of Concerns file_read + csv_retrieve_fields_from.

--- lib/soacsv2mt940/soacsv.rb
+++ lib/soacsv2mt940/soacsv.rb
@@ -10,29 +10,31 @@ module SOACSV2MT940
 
     def file_read
       csv_file = []
-      i = 0
+      row = 0
       if File.exist? @csv_filename
         File.foreach @csv_filename do |record|
-          csv_record = {
-            buchungstag: record.split(';')[0],
-            wertstellung: record.split(';')[1],
-            umsatzart: record.split(';')[2],
-            buchungstext: record.split(';')[3],
-            betrag: record.split(';')[4],
-            waehrung: record.split(';')[5],
-            auftraggeber_konto: record.split(';')[6],
-            auftraggeber_blz: record.split(';')[7],
-            auftraggeber_iban: record.split(';')[8]
-          }
-          csv_file[i] = csv_record
-          i += 1
+          csv_file[row] = csv_retrieve_fields_from(record)
+          row += 1
         end
         csv_file.shift # remove first row (header)
         csv_file.sort_by! { |x| x[:buchungstag] }
-        return csv_file
       else
         raise(StandardError, "File not found: #{@csv_filename}")
-      end
+      end      
+    end
+    
+    def csv_retrieve_fields_from(record)
+      csv_record = {
+        buchungstag: record.split(';')[0],
+        wertstellung: record.split(';')[1],
+        umsatzart: record.split(';')[2],
+        buchungstext: record.split(';')[3],
+        betrag: record.split(';')[4],
+        waehrung: record.split(';')[5],
+        auftraggeber_konto: record.split(';')[6],
+        auftraggeber_blz: record.split(';')[7],
+        auftraggeber_iban: record.split(';')[8]
+      }
     end
   end
 end
