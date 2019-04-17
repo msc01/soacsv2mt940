c3c846358893e53dca8e7e11ec7a39874258c249
Michael Schwarze <michael@schwarze-web.de>
2019-04-17 21:37:17 +0200

    v0.1.11: Updates README.md re. 30t of February bug fix.

--- lib/soacsv2mt940/version.rb
+++ lib/soacsv2mt940/version.rb
@@ -2,5 +2,6 @@ module SOACSV2MT940
   # - v0.1.8: Fixes bug #652 and refactors SOACSV.
   # - v0.1.9: Introduces .CSV file structure as Struct in SOACSV.
   # - v0.1.10: Fixes bug #792: error whith csv fields containing nil.
-  VERSION = '0.1.10'.freeze
+  # - v0.1.11: Updates README.md re. 30t of February bug fix.
+  VERSION = '0.1.11'.freeze
 end
