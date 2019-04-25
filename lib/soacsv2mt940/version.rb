17090ddcc6cac61cd21fe500ab02894e3e1c687d
Michael Schwarze <michael@schwarze-web.de>
2019-04-25 21:49:02 +0200

    v0.2.1: Cleanup temporary directtories.

--- lib/soacsv2mt940/version.rb
+++ lib/soacsv2mt940/version.rb
@@ -3,6 +3,7 @@ module SOACSV2MT940
   # - v0.1.9: Introduces .CSV file structure as Struct in SOACSV.
   # - v0.1.10: Fixes bug #792: error whith csv fields containing nil.
   # - v0.1.11: Updates README.md re. 30th of February bug fix.
-  # - v0.2.0: Handles 1822direktBank .csv file format (issue #4).
-  VERSION = '0.2.0'.freeze
+  # - v0.2.0: Add 1822direktBank .csv file format (issue #4)
+  # - v0.2.1: Cleanup temporary directtories.
+  VERSION = '0.2.1'.freeze
 end
